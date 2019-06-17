abec53eb6cc5a306f02a46db46529b7a690b3de9
OttaviaFeletig <ottafel@hotmail.it>
2019-06-17 19:19:28 +0200

    camera functionality added

--- components/Camera.js
+++ components/Camera.js
@@ -1,20 +1,45 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import { Camera } from 'expo-camera';
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import * as Permissions from "expo-permissions";
+import { Camera } from "expo-camera";
+
+import { Ionicons } from "@expo/vector-icons";
+// import * as FileSystem from "expo-file-system";
+// import ReportIssueSecond from "../screens/ReportIssueScreen";
 
 export default class CameraExample extends React.Component {
   state = {
     hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
+    type: Camera.Constants.Type.back
   };
 
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
+    this.setState({ hasCameraPermission: status === "granted" });
   }
 
-  
+  async snapPhoto() {
+    console.log("Button Pressed");
+    // console.log(this.camera);
+    if (this.camera) {
+      console.log("Taking photo");
+      const options = {
+        quality: 0,
+        base64: true,
+        fixOrientation: true,
+        exif: true
+      };
+      // console.log(options);
+      await this.camera.takePictureAsync(options).then(photo => {
+        photo.exif.Orientation = 1;
+        console.log(photo.uri);
+        //HERE I WILL DISPATCH AN ACTION TO THE REDUX STORE
+        // console.log(FileSystem.cacheDirectory);
+        // const { navigate } = this.props.navigation;
+        // navigate("ReportIssueSecond", { picture: photo.uri });
+      });
+    }
+  }
   render() {
     const { hasCameraPermission } = this.state;
     if (hasCameraPermission === null) {
@@ -24,30 +49,48 @@ export default class CameraExample extends React.Component {
     } else {
       return (
         <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
+          <Camera
+            style={{ flex: 1 }}
+            type={this.state.type}
+            ref={ref => {
+              this.camera = ref;
+            }}
+          >
             <View
               style={{
                 flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-              </TouchableOpacity>
-            </View>
+                backgroundColor: "transparent",
+                flexDirection: "row"
+              }}
+            />
+            <TouchableOpacity onPress={this.snapPhoto.bind(this)}>
+              <View style={{ alignItems: "center" }}>
+                <Ionicons
+                  name="md-radio-button-off" // This is the icon which should take and save image
+                  style={{ color: "white", fontSize: 100 }}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flex: 0.1,
+                alignSelf: "flex-start",
+                alignItems: "center"
+              }}
+              onPress={() => {
+                this.setState({
+                  type:
+                    this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back
+                });
+              }}
+            >
+              <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+                {" "}
+                Flip{" "}
+              </Text>
+            </TouchableOpacity>
           </Camera>
         </View>
       );
---
+++ screens/ReportIssueScreen.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+import { Text, View, Image, StyleSheet } from "react-native";
+import * as FileSystem from "expo-file-system";
+import Camera from "../components/Camera";
+// export default class ReportIssueScreen extends Component {
+export default class ReportIssueScreen extends React.Component {
+  state = {
+    pictureURI: null
+  };
+
+  // componentDidMount() {
+  //   // this.getImageFromCache();
+  // }
+
+  // async getImageFromCache() {
+  //   const image = this.props.navigation.state.params.picture;
+  //   console.log(this.props.navigation.state.params);
+  //   await this.setState({
+  //     pictureURI: image
+  //   });
+  //   console.log(this.state.pictureURI);
+  // }
+  render() {
+    if (this.state.pictureURI == null) {
+      return (
+        <View style={styleCamera}>
+          <Camera />
+        </View>
+
+        // <View style={{ flex: 1, alignItems: "center" }}>
+        //   <Image
+        //     style={{ width: 200, height: 200 }}
+        //     source={{ uri: this.state.pictureURI }}
+        //   />
+
+        //   <Text>hello</Text>
+        // </View>
+      );
+    }
+    // return (
+    //   <View style={{ width: 200, height: 200 }}>
+    //     <Camera />
+    //   </View>
+
+    //   // <View style={{ flex: 1, alignItems: "center" }}>
+    //   //   <Image
+    //   //     style={{ width: 200, height: 200 }}
+    //   //     source={{ uri: this.state.pictureURI }}
+    //   //   />
+
+    //   //   <Text>hello</Text>
+    //   // </View>
+    // );
+  }
+}
+const styleCamera = {
+  width: "100%",
+  height: "100%"
+};
+ReportIssueScreen.navigationOptions = {
+  title: "Report Issue"
+};
