78076f51f38de8e5729a216c5120474efb3d6f63
OttaviaFeletig <ottafel@hotmail.it>
2019-06-25 21:22:48 +0200

    post issue

--- redux/actions/issuesActions.js
+++ redux/actions/issuesActions.js
@@ -1,86 +1,123 @@
-import axios from 'axios';
-import { SET_RADIUS } from './actionTypes'
-import { SET_MARKER } from './actionTypes'
-import { CLEAR_MARKER } from './actionTypes'
-import { GET_ISSUES } from './actionTypes'
-import { ISSUES_LOADING } from './actionTypes'
-import { GET_ISSUES_ERROR } from './actionTypes'
-import { START_TIMER } from 'redux-timer';
-import { getLocation } from './locationActions'
+import axios from "axios";
+import { SET_RADIUS } from "./actionTypes";
+import { SET_MARKER } from "./actionTypes";
+import { CLEAR_MARKER } from "./actionTypes";
+import { GET_ISSUES } from "./actionTypes";
+import { ISSUES_LOADING } from "./actionTypes";
+import { GET_ISSUES_ERROR } from "./actionTypes";
+import { START_TIMER } from "redux-timer";
+import { getLocation } from "./locationActions";
 
-
-
-
-
-
-export const setRadius = (radius) => {
-    return (dispatch) => {
-        dispatch({
-            type: SET_RADIUS,
-            payload: radius
-        })
-
-    }
-}
-export const setMarker = (marker) => {
-    return (dispatch) => {
-        dispatch({
-            type: SET_MARKER,
-            payload: marker
-        })
-
-    }
-}
+export const setRadius = radius => {
+  return dispatch => {
+    dispatch({
+      type: SET_RADIUS,
+      payload: radius
+    });
+  };
+};
+export const setMarker = marker => {
+  return dispatch => {
+    dispatch({
+      type: SET_MARKER,
+      payload: marker
+    });
+  };
+};
 export const clearMarker = () => {
-    return (dispatch) => {
-        dispatch({
-            type: CLEAR_MARKER,
-
-        })
-
-    }
-}
+  return dispatch => {
+    dispatch({
+      type: CLEAR_MARKER
+    });
+  };
+};
 //Cities loading
 export const getIssuesLoading = () => {
-    return {
-        type: ISSUES_LOADING
-    };
+  return {
+    type: ISSUES_LOADING
+  };
 };
 export const getIssues = () => {
-    return (dispatch, getState) => {
-        dispatch(getIssuesLoading());
+  return (dispatch, getState) => {
+    dispatch(getIssuesLoading());
 
-        const state = getState();
-        const region = state.location.USER_POSITION
-        const token = state.user.USER.token
+    const state = getState();
+    const region = state.location.USER_POSITION;
+    const token = state.user.USER.token;
 
-        //const radius = state.issues.RADIUS  //radius in m
-        const radius = state.issues.RADIUS / 1000 //radius in km
+    //const radius = state.issues.RADIUS  //radius in m
+    const radius = state.issues.RADIUS / 1000; //radius in km
 
-        const URL = `http://kietz.herokuapp.com/api/issues?latitude=${region.latitude}
-        &longitude=${region.longitude}&page=0&radius=${radius}`
-        console.log(URL);
-        return axios.get(URL, { headers: { "Authorization": `Bearer ${token}` } })
-            .then((res) => {
-                console.log(res.data);
-                dispatch({
-                    type: GET_ISSUES,
-                    payload: res.data
-                })
-            }).catch((err) => {
-                console.log(err)
-                dispatch({
-                    type: GET_ISSUES_ERROR,
-                    payload: err
-                })
-            })
+    const URL = `http://kietz.herokuapp.com/api/issues?latitude=${
+      region.latitude
     }
+        &longitude=${region.longitude}&page=0&radius=${radius}`;
+    console.log(URL);
+    return axios
+      .get(URL, { headers: { Authorization: `Bearer ${token}` } })
+      .then(res => {
+        console.log(res.data);
+        dispatch({
+          type: GET_ISSUES,
+          payload: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch({
+          type: GET_ISSUES_ERROR,
+          payload: err
+        });
+      });
+  };
+};
 
-}
+export const postIssue = issue => {
+  return async (dispatch, getState) => {
+    const URL = "http://kietz.herokuapp.com/api/issues";
+    const state = getState();
+    const { token } = state.user.USER;
 
+    const string2 =
+      '{\n    "category": "' +
+      issue.category +
+      '",\n    "description": "Trash talk",\n    "imageUrls": [\n        "' +
+      issue.imageUrls +
+      '"\n    ],\n    "latitude": ' +
+      issue.location.latitude +
+      ',\n    "longitude": ' +
+      issue.location.longitude +
+      "\n}";
 
+    const rawResponse = await fetch(URL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: string2
+    });
+    // const string = `{\n\t\"category\": \"${
+    //   issue.category
+    // }\",\n\t\"imgUrls\": \"[${issue.imageUrls}]\",
+    // \n\t\"location\": {\n\t\"longitude\": \"${issue.location.longitude}\",
+    // \n\t\"latitude\": \"${issue.location.latitude}\"\n\t\}
+    // }`;
 
+    // console.log(string);
+    //     console.log("state in post issue", state);
+    //     console.log("issue in post issue", issue);
+    //     return axios
 
+    //       .post(URL, {
+    //         headers: { Authorization: `Bearer ${token}` },
+    //         body: string2
+    //       })
+    //       .then(res => console.log("issue posted", res))
+    //       .catch(err => console.log("error", err));
+  };
+};
 
 // export const startTimer = () => {
 //     return (dispatch, getState) => {
--- screens/ReportIssueScreen.js
+++ screens/ReportIssueScreen.js
@@ -1,156 +1,191 @@
 import React, { Component } from "react";
-
-import { Text, View, Image, StyleSheet, TextInput, Button, ProgressBarAndroid } from "react-native";
+import { SecureStore } from "expo";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  TextInput,
+  Button,
+  ProgressBarAndroid
+} from "react-native";
 import Colors from "../constants/Colors";
 import Camera from "../components/Camera";
 import { connect } from "react-redux";
-import axios from "axios";
+
+import { postIssue } from "../redux/actions/issuesActions";
 
 import * as firebase from "firebase";
 
 class ReportIssueScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            progress: 0,
-            photoURL: null,
-            photoUploading: false,
-            // pictureURI: null,
-            category: "Traffic Light"
-        }
-    }
-    componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: 0,
+      photoURL: null,
+      photoUploading: false,
+      // pictureURI: null,
+      category: "Traffic Light"
+    };
+  }
+  componentDidMount() {}
 
+  submitIssue = () => {
+    const { PICTURE_FILE } = this.props.pictureURI;
+    const { PICTURE_LOCATION } = this.props.pictureURI;
+    const { PICTURE_LOADER } = this.props.pictureURI;
+
+    console.log("FILE IN REPORT ISSUE" + PICTURE_FILE);
+    this.setState({ photoUploading: true });
+
+    fetch(PICTURE_FILE).then(res => {
+      this.getPictureLink(res);
+    });
+  };
+  getPictureLink = res => {
+    const storageService = firebase.storage();
+    const storageRef = storageService.ref();
+    const { PICTURE_LOCATION } = this.props.pictureURI;
+    const { ADDRESS } = this.props.location;
+    const id = `${ADDRESS.city}-${this.state.category}-${new Date()}`;
+    console.log(id);
+    console.log(res);
+    const blob = res._bodyBlob;
+
+    const uploadTask = storageRef.child(`issues/${id}`).put(blob);
+    uploadTask.on(
+      "state_changed",
+      snapshot => {
+        let progress = Math.round(
+          (snapshot.bytesTransferred * 100) / snapshot.totalBytes
+        );
+        this.setState({ progress: progress / 100 });
+        console.log(progress);
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        console.log("success");
+        this.setState({ photoUploading: false });
+        firebase
+          .storage()
+          .ref("issues")
+          .child(`${id}`)
+          .getDownloadURL()
+          .then(photoURL => {
+            console.log(photoURL);
+            this.setState({ photoURL });
+            const issue = {
+              imageUrls: [photoURL],
+              location: {
+                longitude: PICTURE_LOCATION.longitude,
+                latitude: PICTURE_LOCATION.latitude
+              },
+              category: this.state.category
+            };
+            this.props.postIssue(issue);
+          });
+      }
+    );
+  };
+  render() {
+    console.log(this.props.pictureURI);
+    const { PICTURE_FILE } = this.props.pictureURI;
+    const { PICTURE_LOADER } = this.props.pictureURI;
+    if (PICTURE_FILE == null) {
+      return (
+        <View style={styleCamera}>
+          <Camera />
+        </View>
+      );
     }
 
-    submitIssue = () => {
-        const { PICTURE_FILE } = this.props.pictureURI;
+    //else if category has not been choosen yet, render category component
+    // else if (this.props.pictureURI.PICTURE_LOADER) {
+    //   return <Loader />;
+    // }
+    else {
+      if (this.props.pictureURI.PICTURE_LOCATION == null) {
+        return <Text>Loading..</Text>;
+      } else {
         const { PICTURE_LOCATION } = this.props.pictureURI;
-        const { PICTURE_LOADER } = this.props.pictureURI;
-        const { ADDRESS } = this.props.location
-        const storageService = firebase.storage();
-        const storageRef = storageService.ref();
-        console.log("FILE IN REPORT ISSUE" + PICTURE_FILE);
-        this.setState({ photoUploading: true })
+        // const { username } = this.props.user.USER;
+        // const { token } = this.props.user.USER;
+        // console.log(username);
+        // console.log(token);
+        return (
+          <View style={{ flex: 1, alignItems: "center" }}>
+            <Image style={stylePicture} source={{ uri: PICTURE_FILE }} />
+            <Text>Selected Issue</Text>
+            <View style={styleCategory}>
+              <Text
+                style={{ textAlign: "center", fontSize: 20, color: "white" }}
+              >
+                {this.state.category}
+              </Text>
+            </View>
 
-        const id = `${ADDRESS.city}-${this.state.category}-${new Date()}`
-        console.log(id)
-        fetch(PICTURE_FILE).then(res => {
-            console.log(res);
-            const blob = res._bodyBlob
-
-            const uploadTask = storageRef
-                .child(`issues/${id}`)
-                .put(blob);
-            uploadTask.on(
-                "state_changed",
-                snapshot => {
-
-                    let progress = Math.round(snapshot.bytesTransferred * 100 / snapshot.totalBytes)
-                    this.setState({ progress: progress / 100 });
-                    console.log(progress);
-
-                },
-                error => {
-                    console.log(error);
-                },
-                () => {
-                    console.log("success");
-                    this.setState({ photoUploading: false })
-                    firebase
-                        .storage()
-                        .ref("issues")
-                        .child(`${id}`)
-                        .getDownloadURL()
-                        .then(photoURL => this.setState({ photoURL }));
-                }
-            );
-        })
+            <Text>Comment</Text>
+            <TextInput style={styleInput} />
+            <Button
+              onPress={this.submitIssue}
+              title="SUBMIT"
+              color="#841584"
+              accessibilityLabel="SUBMIT"
+            />
+            <ProgressBarAndroid
+              styleAttr="Horizontal"
+              indeterminate={false}
+              progress={this.state.progress}
+            />
+          </View>
+        );
+      }
     }
-    render() {
-        console.log(this.props.pictureURI);
-        const { PICTURE_FILE } = this.props.pictureURI;
-        const { PICTURE_LOADER } = this.props.pictureURI;
-        if (PICTURE_FILE == null) {
-            return (
-                <View style={styleCamera}>
-                    <Camera />
-                </View>
-            );
-        }
-
-        //else if category has not been choosen yet, render category component
-        // else if (this.props.pictureURI.PICTURE_LOADER) {
-        //   return <Loader />;
-        // }
-        else {
-            if (this.props.pictureURI.PICTURE_LOCATION == null) {
-                return <Text>Loading..</Text>;
-            } else {
-                const { PICTURE_LOCATION } = this.props.pictureURI;
-                return (
-                    <View style={{ flex: 1, alignItems: "center" }}>
-                        <Image style={stylePicture} source={{ uri: PICTURE_FILE }} />
-                        <Text>Selected Issue</Text>
-                        <View style={styleCategory}>
-                            <Text
-                                style={{ textAlign: "center", fontSize: 20, color: "white" }}
-                            >
-                                {this.state.category}
-                            </Text>
-                        </View>
-
-                        <Text>Comment</Text>
-                        <TextInput style={styleInput} />
-                        <Button
-                            onPress={this.submitIssue}
-                            title="SUBMIT"
-                            color="#841584"
-                            accessibilityLabel="SUBMIT"
-                        />
-                        <ProgressBarAndroid
-                            styleAttr="Horizontal"
-                            indeterminate={false}
-                            progress={this.state.progress}
-                        />
-                    </View>
-                );
-            }
-        }
-    }
+  }
 }
 const styleCamera = {
-    width: "100%",
-    height: "100%"
+  width: "100%",
+  height: "100%"
 };
 
 const stylePicture = {
-    width: "70%",
-    height: "50%"
+  width: "70%",
+  height: "50%"
 };
 
 const styleCategory = {
-    width: "70%",
-    fontSize: 20,
-    margin: 5,
-    backgroundColor: "#841584"
+  width: "70%",
+  fontSize: 20,
+  margin: 5,
+  backgroundColor: "#841584"
 };
 
 const styleInput = {
-    width: "80%",
-    height: 100,
-    borderColor: "gray",
-    borderWidth: 1
+  width: "80%",
+  height: 100,
+  borderColor: "gray",
+  borderWidth: 1
 };
 
 ReportIssueScreen.navigationOptions = {
-    title: "Report Issue"
+  title: "Report Issue"
 };
 const mapStateToProp = state => {
-    return {
-        pictureURI: state.pictureURI,
-        location: state.location
-    };
+  return {
+    pictureURI: state.pictureURI,
+    location: state.location,
+    user: state.user
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    postIssue: issue => dispatch(postIssue(issue))
+  };
 };
 
-export default connect(mapStateToProp)(ReportIssueScreen);
+export default connect(
+  mapStateToProp,
+  mapDispatchToProps
+)(ReportIssueScreen);
