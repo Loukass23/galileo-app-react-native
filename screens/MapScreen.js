048d63ea8bb19e61c228dc99b177917f47442b0f
Lucas D <l.dupias@net.estia.fr>
2019-06-26 23:25:20 +0200

    Home screen styling

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -15,7 +15,7 @@ const markerImages = {
   Trash: require('../assets/images/Marker1.png'),
   Road: require('../assets/images/Marker2.png'),
   Vandalism: require('../assets/images/Marker3.png'),
-  Category4: require('../assets/images/Marker4.png'),
+  Traffic: require('../assets/images/Marker4.png'),
   Category5: require('../assets/images/Marker5.png'),
 
 };
